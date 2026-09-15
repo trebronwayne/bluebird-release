a2723db4b6223ee6b9f3c7eb08d1c841ca69ea1f
Nobert <trebronwayne@gmail.com>
2026-09-15 15:44:46 +0300

    Make Pages build reproducible

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/bluebird-release/' : '/'
+  base: process.env.GITHUB_ACTIONS ? '/bluebird-release/' : '/',
+  build: {
+    rollupOptions: {
+      input: 'site.html'
+    }
+  }
 });
