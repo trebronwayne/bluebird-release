389190d9ca8d613ebfd3768ee5c8420cbc2196ec
Nobert <trebronwayne@gmail.com>
2026-09-16 06:02:34 +0300

    Make the release-site build reproducible

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/bluebird-release/' : '/',
-  build: {
-    rollupOptions: {
-      input: 'site.html'
-    }
-  }
+  base: process.env.GITHUB_ACTIONS ? '/bluebird-release/' : '/'
 });
